8125537a6d647cf6de6f0e72036aba23ece343f3
Adam Starikiewicz <kewkie@gmail.com>
2013-01-22 23:04:08 +0000

    Adding structuremap as the dependency resolver and configuring it to inject ClimbrContext to the controllers

---
+++ Web/Common/ClimbrController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Domain;
+
+namespace Web.Common
+{
+    public class ClimbrController : Controller
+    {
+        protected readonly ClimbrContext db;
+
+        public ClimbrController(ClimbrContext context)
+        {
+            db = context;
+        }
+    }
+}
